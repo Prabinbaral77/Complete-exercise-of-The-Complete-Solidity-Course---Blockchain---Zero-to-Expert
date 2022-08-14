ed70c629b2b0af264d309c7033059dcf1916ea88
Prabinbaral77 <prabin.baral00@gmail.com>
2022-08-14 18:27:59 +0545

    variable in solidity

---
+++ 002_variables.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/*
+      variable:
+      --reserved memeory location to store values.
+      --when you create a variable you reserve some space in memory.
+      TYPES:
+      --Boolean /true or false eg: lieDetector
+      --String --order sequence of charactar eg: "hello john"
+      --Integer eg: walletAmount
+ */
+
+// contract Variables {
+//     bool is_present;
+//     string name;
+//     uint age;
+
+//     constructor() {
+//         is_present = true;
+//         name= "JOHN";
+//         age = 15;
+//     }
+// }
+
