39505e39d6ff8eba2df4cae535b0bf5a25d1f80a
Prabinbaral77 <prabin.baral00@gmail.com>
2022-08-15 18:34:17 +0545

    variable excercise done

--- 002_variables.sol
+++ 002_variables.sol
@@ -13,14 +13,19 @@ pragma solidity >=0.7.0 <0.9.0;
  */
 
 // contract Variables {
-//     bool is_present;
-//     string name;
-//     uint age;
 
-//     constructor() {
-//         is_present = true;
-//         name= "JOHN";
-//         age = 15;
-//     }
+//    uint chocolateBar = 10;
+//    uint storeOwner = 300;
+//    bool lieDetector; //default false
+//    string errorMessage = "Error, something went wrong!";
+
 // }
 
+
+//Ex-1. create new variable called wallet as integer, create boolean called spend, create a string give it a name notifySpend, initialize wallet to 500, set value of spent to false, add a string literal = "you have spent money" to notifyspend 
+
+contract VariableExercise{
+    uint wallet = 500;
+    bool spend;
+    string notifySpend = "You have spend money";
+}
