578f97dd6ca362325a7e346f2309c9a8c7d13a4c
Prabinbaral77 <prabin.baral00@gmail.com>
2022-08-16 18:29:50 +0545

    function exercise done

--- 003_function.sol
+++ 003_function.sol
@@ -4,9 +4,11 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /*
   Function: group of reuseable code that we can used anywhere in the program.
+  function takes the input of information and produce output.
   function functionName(parameter list) scope returns() {
     ...statement
   }  
+  scope is a visibility. either private, public, internal and external
  */
 
 
@@ -17,6 +19,14 @@ pragma solidity >=0.7.0 <0.9.0;
    remoteControlClose: close the door;  - function
  */
 
+
+ /*
+    view indicates that the function will not alter the storage state in any way. But it allows you to "view" it
+
+    pure is even more strict, indicating that it will not even read the storage state.
+  */
+
+
  contract ControlSystem {
     bool public isDoorOpen;
 
@@ -26,6 +36,28 @@ pragma solidity >=0.7.0 <0.9.0;
 
     function remoteControlClose() public {
         isDoorOpen = false;
+    }
+
+    //local variable superseed(overlapped) state variable
+    //uint sum = 10; //state variable
+    //create a function that add two variable
+    function addNumber(uint num1, uint num2) public pure returns(uint) {
+      //all the variable we write into the function remain localized inside function scope.
+      uint sum = num1 + num2;  //here sum is local variable
+      return sum;
+    }
+
+
+    uint num = 5;
+    //exercise: create the function which performs multiplication.
+    function multiplyCalculator(uint num1, uint num2) public pure returns(uint) {
+      uint result = num1 * num2;
+      return result;
+    }
+
+    function divideCalculator(uint num1) public view returns(uint) {
+      uint result = num1 / num;
+      return result;
     }
  }
 
