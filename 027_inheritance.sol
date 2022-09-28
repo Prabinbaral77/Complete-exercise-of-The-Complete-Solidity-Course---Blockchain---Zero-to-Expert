20dec35d0ce42f7fb31552ce3e268e534efcd9a9
Prabinbaral77 <prabin.baral00@gmail.com>
2022-09-28 13:38:42 +0545

    inheritance and override done

--- 027_inheritance.sol
+++ 027_inheritance.sol
@@ -41,3 +41,65 @@ contract B is A {
         return innerVal;
     }
 }
+
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // uint256[] public anArray;
+    People[] public people;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
+    
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
+
+//while importing the contract to another contract 
+// 1. copy and paste the contract in current solidity file
+// 2. import the contract if lots of contract are required for the computation like:=        import "./filepath/filename.sol"
+
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageArray; 
+    //initial contract address 0x0000.......000 after createSimpleStorageContract function is called contract address 0xrte46gdfg5..........jhg4j
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage(); //new deploys the contract from other contract
+        simpleStorageArray.push(simpleStorage);  
+    }
+
+    function sfStore(uint _simpleStorageIndex, uint _simpleStorageNumber) public {
+        //ABI = application binary interface or using address
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+
+    }
+
+    function sfGet(uint _simpleStorageIndex) public view returns(uint) {
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+}
+
+//if we want most of the functionalities of simplestorage smart contract without some functionalities we inherit the smart contract and 
+// overrides the function in the prev contract with new implementation.
+contract ExtraStorage is SimpleStorage {
+    //we want to store function to add 5 to the favourite number
+    function store(uint _favoriteNumber) public override {   //to override the function we need to write virtual in original function that going to override
+        favoriteNumber = _favoriteNumber + 5;
+    }
+}
